ca7bc86d52aec79cb71b16c823a50e5737ad0c9f
JamesKha <57509095+JamesKha@users.noreply.github.com>
2020-03-21 14:34:25 -0700

    Update Candy.java

--- 166HW4/Candy.java
+++ 166HW4/Candy.java
@@ -1,8 +1,27 @@
 
-public class Candy {
+public class Candy extends DessertItem{
+	private String typeOfCandy; 
+	private double howManyPounds;
+	private int pricePerPound; 
+	
+	public Candy(String newTypeOfCandy, double howManyPounds, int newPricePerPound){
+		this.typeOfCandy = newTypeOfCandy; 
+		this.howManyPounds = howManyPounds; 
+		this.pricePerPound = newPricePerPound; 
+	}
+	public int getCost(){
+		return (int) Math.round(pricePerPound* howManyPounds); 
+	}
 
-	public Candy() {
-		// TODO Auto-generated constructor stub
+public String toString() {
+	return this.howManyPounds + " @ " + this.pricePerPound+ "/dz\n" + this.typeOfCandy + " " + this.getCost(); 
+	}
+
+
+
+public static void main(String[] args) {
+	DessertItem candy1 = new Candy("fudge", 2.30, 89); 
+	System.out.println(candy1.toString());
 	}
 
 }
