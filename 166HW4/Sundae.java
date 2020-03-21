2d2ce758d15d023c593ed743bc2b7a193d0367f5
JamesKha <57509095+JamesKha@users.noreply.github.com>
2020-03-21 13:09:55 -0700

    Update Sundae.java

--- 166HW4/Sundae.java
+++ 166HW4/Sundae.java
@@ -1,8 +1,26 @@
 
-public class Sundae {
+public class Sundae extends IceCream{
+private String typeOfTopping;
+private int costOfTopping; 
+IceCream exampleSundae = new IceCream(); 
 
-	public Sundae() {
-		// TODO Auto-generated constructor stub
-	}
 
+public Sundae(String newTypeOfIceCream, int newcostOfIceCream, String newTypeOfTopping,  int newCostOfTopping) {
+	exampleSundae.setTypeOfIceCream(newTypeOfIceCream);
+	exampleSundae.setHowMuchPerServing(newcostOfIceCream);
+	this.typeOfTopping = newTypeOfTopping; 
+	this.costOfTopping = newCostOfTopping; 
 }
+public int getCost(){
+	return (this.getCost() + this.costOfTopping); 
+}
+
+public String getTypeOfIceCream() {
+	return typeOfIceCream; 
+}
+
+
+ public String toString() { 
+	 return this.typeOfTopping + " Sundae with " + exampleSundae.getTypeOfIceCream(); 
+	 }
+	 
