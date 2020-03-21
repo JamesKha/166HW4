0cbefebc2496a15f4037b8fb83c5d76b92b681f6
JamesKha <57509095+JamesKha@users.noreply.github.com>
2020-03-21 13:08:47 -0700

    Update IceCream.java
    March 21/2020

--- 166HW4/IceCream.java
+++ 166HW4/IceCream.java
@@ -1,8 +1,37 @@
+public class IceCream extends DessertItem{
+protected String typeOfIceCream; 
+private int howMuchPerServing;
 
-public class IceCream {
+public IceCream(String newTypeOfIceCream, int newHowMuchPerServing) {
+	this.typeOfIceCream = newTypeOfIceCream; 
+	this.howMuchPerServing = newHowMuchPerServing; 
+}
 
-	public IceCream() {
-		// TODO Auto-generated constructor stub
-	}
+public int getCost(){
+	return this.howMuchPerServing; 
+}
 
+public String getTypeOfIceCream(){
+	return this.typeOfIceCream; 
+}
+
+public int getHowMuchPerServing() {
+	return howMuchPerServing;
+}
+
+public void setHowMuchPerServing(int howMuchPerServing) {
+	this.howMuchPerServing = howMuchPerServing;
+}
+
+public void setTypeOfIceCream(String typeOfIceCream) {
+	this.typeOfIceCream = typeOfIceCream;
+}
+
+public IceCream() {
+	
+}
+
+public String toString() {
+	return this.typeOfIceCream; 
+}
 }
