04d439ae579e2d3a5a4c204a4ca7340e0c52a4f5
JamesKha <57509095+JamesKha@users.noreply.github.com>
2020-03-22 14:17:21 -0700

    Added some Java Documentation

--- 166HW4/Cookie.java
+++ 166HW4/Cookie.java
@@ -10,17 +10,29 @@ private String typeOfCookie;
 private int numberOfCookies; 
 private int costPerCookieDozen; 
 
-
+/**
+ * @param newTypeOfCookie
+ * @param newNumberOfCookies
+ * @param newCostPerCookieDozen
+ */
 public Cookie(String newTypeOfCookie, int newNumberOfCookies, int newCostPerCookieDozen) {
 this.typeOfCookie = newTypeOfCookie; 
 this.numberOfCookies = newNumberOfCookies;
 this.costPerCookieDozen = newCostPerCookieDozen;
 }
+/**
+ *takes the amount for a dozen cookies listed within the parameter of the constructor and calculating how much is a single cookie
+ *@return the cost with the amount cookies needed adjusted to the single cookie price 
+ */
 public int getCost(){
 	int perEachCookie = costPerCookieDozen/12; 
 	return perEachCookie* numberOfCookies; 
 }
 
+/**
+ * Creates a string format depending on the parameters listed that are specific to the cookies: typeOfCookie, numberOfCookies, costPerCookieDozen
+ *@return a string that includes the parameters and the due cost of the .getCost
+ */
 public String toString() {
 	return this.numberOfCookies + " @ " + (double) this.costPerCookieDozen / (double) 100 + "/dz.\n" 
 			+ String.format("%1$-" + DessertShoppe.MAX_ITEM_NAME_SIZE + "s"+ "%2$"
@@ -32,4 +44,3 @@ public static void main(String[] args) {
 	System.out.println(cookie1.toString());
 }
 }
-
